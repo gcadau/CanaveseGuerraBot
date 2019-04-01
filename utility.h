3193f779b7e93f7e63339b13747c40fd7a16e2bb
gcad96 <48099775+gcad96@users.noreply.github.com>
2019-04-01 22:39:18 +0200

    Update utility.h

--- utility.h
+++ utility.h
@@ -5,7 +5,7 @@
 #define N 300
 
 //FILES:
-#define FILE_DATI "C:\\Users\\Giovanni\\CLionProjects\\untitled46\\centroidi_comuni.csv.txt"
-#define FILE_NOMI "C:\\Users\\Giovanni\\CLionProjects\\untitled46\\Canavese.txt"
+#define FILE_DATI "centroidi_comuni.csv.txt"
+#define FILE_NOMI "Canavese.txt"
 
 #endif //UTILITY_H
