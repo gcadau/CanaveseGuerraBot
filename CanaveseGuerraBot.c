052f4fb2848f11869b04b4fd7bf7713798c95ca6
gcad96 <48099775+gcad96@users.noreply.github.com>
2019-04-04 11:44:16 +0200

    Update CanaveseGuerraBot.c

--- CanaveseGuerraBot.c
+++ CanaveseGuerraBot.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 #include <string.h>
 #include "utility.h"
 #include "comune.h"
 #include "comuni.h"
 #include "CanaveseGuerraBot.h"
+
+#define CONQUISTA 'C'
 
 void Game()
 {
@@ -13,10 +16,25 @@ void Game()
 
     calcolaDistanze(comuni);
 
-    conquista(comuni);
+    while(mossa())      conquista(comuni);
 
     stampaClassifica(comuni);
     stampaComuniIndipendenti(comuni);
 
     liberaComuni(comuni);
 }
+          
+          
+int mossa()
+{
+    char r; 
+    scanf("%c", &r);
+    scanf("%*c");
+    r = toupper(r); 
+    
+    if(r==CONQUISTA)    return 1; 
+    
+    return 0; 
+}
+        
+    
