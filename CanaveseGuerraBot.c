e948c2dbcd50b327b8b15a613661da6677a1ed98
gcad96 <48099775+gcad96@users.noreply.github.com>
2019-04-04 19:35:08 +0200

    Update CanaveseGuerraBot.c

--- CanaveseGuerraBot.c
+++ CanaveseGuerraBot.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "utility.h"
-#include "casualita.h>
+#include "casualita.h"
 #include "comune.h"
 #include "comuni.h"
 #include "CanaveseGuerraBot.h"
