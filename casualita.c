e7f1fa64ee0015f4a59559d36207a25e8f9c9d8c
gcad96 <48099775+gcad96@users.noreply.github.com>
2019-04-10 15:58:18 +0200

    Add files via upload

--- casualita.c
+++ casualita.c
@@ -6,15 +6,43 @@
 #define PROBABILITA 40
 #define SUCCESSO 20
 
+#define DIM 10000
+
+#define FILE_IN "randomOffset.txt"
+
+
 int count = 0;
+
+int* offset; int n = 0;
+
+int set = 0;
 
 int generaNumeroCasuale(int range)
 {
+    if(!set)
+    {
+        set = 1;
+
+        offset = malloc(DIM*sizeof(int));
+
+        FILE* f;
+        if((f=fopen(FILE_IN, "r"))==NULL)   return -1;
+        int i;
+        for(i=0; i<DIM; i++)
+        {
+            fscanf(f, "%d", &(offset[i]));
+        }
+        fclose(f);
+    }
+
     int i;
     srand(time(NULL)+count++);
     srand(rand());
 
     i = rand() % range;
+
+    i+=offset[n++];
+    i%=range;
 
     return i;
 }
@@ -22,6 +50,6 @@ int generaNumeroCasuale(int range)
 int generaSceltaCasuale()
 {
     int scelta = generaNumeroCasuale(PROBABILITA+1);
-    
-    return (scelta==SUCCESSO); 
+
+    return (scelta==SUCCESSO);
 }
