ddf2bc22a2878b60c930391e0b51cc726109c40c
gcad96 <48099775+gcad96@users.noreply.github.com>
2019-04-30 10:46:22 +0200

    Add files via upload

---
+++ Contatore.java
@@ -0,0 +1,21 @@
+package utility;
+
+public class Contatore
+{
+    private int posto;
+
+    public Contatore(int p)
+    {
+        this.posto = p;
+    }
+
+    public int getPosto()
+    {
+        return posto;
+    }
+
+    public void setPosto(int posto)
+    {
+        this.posto = posto;
+    }
+}
