8a6bde112fd29f550477fc97fb01f125906a6a59
gcad96 <48099775+gcad96@users.noreply.github.com>
2019-04-30 10:54:16 +0200

    Update Utility.java

--- Utility.java
+++ Utility.java
@@ -2,6 +2,6 @@ package utility;
 
 public class Utility
 {
-    public static final String LUOHI = "Dettaglio_luoghi.csv.txt";
+    public static final String LUOGHI = "Dettaglio_luoghi.csv.txt";
     public static final String DETTAGLI = "Dettagli.txt";
 }
