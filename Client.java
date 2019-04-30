8ca46d5b465d4f62ccc655e13ee98dda3de590ce
gcad96 <48099775+gcad96@users.noreply.github.com>
2019-04-30 16:53:11 +0200

    Update Client.java

--- Client.java
+++ Client.java
@@ -8,14 +8,14 @@ public class Client
 {
     public static void main(String[] args)
     {
-        Nazione italia = Stat.caricaDB(LUOGHI);
+        Nazione italia = Stat.caricaDB(Utility.LUOGHI);
         if(italia==null)
         {
             System.err.println("Errore nel caricamento dati.");
         }
         else
         {
-            if(!Stat.caricaSituazione(italia, situazione))
+            if(!Stat.caricaSituazione(italia, Utility.DETTAGLI))
             {
                 System.err.println("Errore nel caricamento dettagli. Non tutti i comuni sono stati riconosciuti");
             }
